e665f088aea2ac388a8fb575a1271c9140abe0a9
Hannah Phillips <hannah.phillips@simplybusiness.co.uk>
2024-06-13 14:04:03 +0100

    added a placeholder Gemfile and ordered the lib file with the index in it

--- lib/index.rb
+++ lib/index.rb
@@ -34,46 +34,6 @@ type = gets.chomp
 puts "Duration (in days): "
 duration = gets.chomp.to_i
 
-# Generate packing checklist
-# packing_list = []
-
-# packing_list << "Clothes for #{duration} days"
-# packing_list << "Toiletries"
-# packing_list << "Medications"
-# packing_list << "Passports and travel documents"
-# packing_list << "Electronics (chargers, adapters, etc.)"
-# packing_list << "Money and credit cards"
-# packing_list << "Snacks and drinks for the journey"
-# packing_list << "Entertainment (books, games, etc.)"
-
-# if type == "Beach / resort holiday"
-#     packing_list << "Swimsuits"
-#     packing_list << "Beach towels"
-#     packing_list << "Sunscreen"
-# elsif type == "Adventure holiday"
-#     packing_list << "Hiking boots"
-#     packing_list << "Warm clothing"
-#     packing_list << "Trekking poles"
-# elsif type == "Camping holiday"
-#     packing_list << "Tent"
-#     packing_list << "Tent poles"
-#     packing_list << "Sleeping bag"
-#     packing_list << "Hiking boots"
-#     packing_list << "Warm clothing"
-#     packing_list << "Trekking poles"
-# elsif type == "City break"
-#     packing_list << "Smart dress"
-# elsif type == "Travel around multiple destinations"
-#     packing_list << "Swimsuits"
-#     packing_list << "Beach towels"
-#     packing_list << "Sunscreen"
-#     packing_list << "Hiking boots"
-#     packing_list << "Warm clothing"
-#     packing_list << "Smart dress"
-# end
-
-# packing_list << "Other miscellaneous items"
-
 
 # Generate packing checklist in sections (need to create error handling for nil nil class)
 packing_list = {"Important" => [], "Clothing" => [], "Personal care" => [], "Electronics" => [], "Food" => [], "Entertainment" => [], "Miscellaneous" => []}
@@ -114,13 +74,6 @@ elsif type == "Travel around multiple destinations"
 end
 
 packing_list["Miscellaneous"] << "Other miscellaneous items"
-
-
-# Display packing checklist
-# puts "\nHere's your packing checklist for the holiday to #{destination}:"
-# packing_list.each_with_index do |item, index|
-#     puts "#{index + 1}. #{item}"
-# end
 
 # Use name and age variables to create individual packing lists to display
 puts "\nHere's the individual packing list for each person for the holiday to #{destination}:"
